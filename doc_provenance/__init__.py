0307aa17a2d13fa212434ae151794d675371479a
yiminl18 <32020422+yiminl18@users.noreply.github.com>
2025-05-28 16:58:02 -0700

    update apis

--- doc_provenance/__init__.py
+++ doc_provenance/__init__.py
@@ -8,5 +8,6 @@ from .base_strategies import QA
 from .test_provenance import provenance_run
 from .provenance import equal
 from .provenance import set_model
+from .provenance import LLM_vanilla
 
-__all__ = ["divide_and_conquer_progressive_API", "logger", "digest_hotpotQA_dataset","digest_paper_dataset", "sample_paper_questions", "compute_embeddings","QA", "provenance_run", "equal","set_model"]
+__all__ = ["divide_and_conquer_progressive_API", "logger", "digest_hotpotQA_dataset","digest_paper_dataset", "sample_paper_questions", "compute_embeddings","QA", "provenance_run", "equal","set_model","LLM_vanilla"]
